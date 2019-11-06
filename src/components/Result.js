7f7f932f4f8f8da5705227b62e45b3c35fb3a7c9
Caio Silva <caiovosilva@outlook.com>
2019-11-05 22:23:20 -0300

    begun to consume championship endpoint, need to understand CORS better

--- src/components/Result.js
+++ src/components/Result.js
@@ -8,7 +8,17 @@ export default class Result extends Component {
 
     componentDidMount () {
         const { movies } = this.props.location.state
-    
+        //  axios.get('https://gjnskjqty1.execute-api.sa-east-1.amazonaws.com/Prod/api/movies')
+        axios.post('http://localhost:49274/api/championship', movies)
+        .then((winners) => {
+            console.log(winners)
+            // this.setState({
+            //     winners: res.data.winners
+            // })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
     render() {
         console.log(this.props)
